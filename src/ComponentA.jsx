9dbb611aa84f969dfdedc45b0e293ce3db46a35b
ByteMasterSamir <samiradhikary972@gmail.com>
2024-06-29 18:01:00 +0545

    Components import

--- src/ComponentA.jsx
+++ src/ComponentA.jsx
@@ -1,9 +1,10 @@
-
- 
+import ComponentC from "./ComponentC"
+ import ComponentB from "./ComponentB"
  const ComponentA = () => {
    return (
      <div className="box">
        <h1>Component A</h1>
+       <ComponentB />
      </div>
    )
  }
